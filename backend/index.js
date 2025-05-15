149bc5ab9c7d65aba8353fc9c18c0fe4a5cd4706
viniblack <viniibl4ck@gmail.com>
2025-05-15 17:19:27 -0300

    configuring colors

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import axios from 'axios';
+const PORT = process.env.PORT || 5000;
 import { fretePorEstado } from './frete.js';
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({ menssage: 'API Frete' })
